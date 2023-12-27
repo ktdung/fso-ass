fbf97ed26951a26a543858b074865a4831e2921c
ktdung <ktdunglibre@gmail.com>
2023-12-27 19:50:35 +0700

    [unicafe] 1.8 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,22 @@
 import { useState } from 'react';
 
+const Statistics = ({ good = 0, bad = 0, neutral = 0 }) => {
+  return (
+    <>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>
+        average{' '}
+        {(good + neutral * 0 + bad * -1) / (good + neutral + bad)}
+      </p>
+      <p>positive {good / (good + neutral + bad)} %</p>
+    </>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -11,16 +28,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>
-        average{' '}
-        {(good + neutral * 0 + bad * -1) / (good + neutral + bad)}
-      </p>
-      <p>positive {good / (good + neutral + bad)} %</p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
