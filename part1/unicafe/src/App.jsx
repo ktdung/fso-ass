4bb28a893b7888247f9e4545f77a61f8509b445a
ktdung <ktdunglibre@gmail.com>
2023-12-27 20:00:42 +0700

    [unicafe] 1.9 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 const Statistics = ({ good = 0, bad = 0, neutral = 0 }) => {
   return (
     <>
-      <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
@@ -28,7 +27,12 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics good={good} bad={bad} neutral={neutral} />
+      <h2>statistics</h2>
+      {good !== 0 || neutral !== 0 || bad !== 0 ? (
+        <Statistics good={good} bad={bad} neutral={neutral} />
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
