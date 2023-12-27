c227a8d960c9bbad670f32fdc5ea81c4b676a3bf
ktdung <ktdunglibre@gmail.com>
2023-12-27 19:47:08 +0700

    [unicafe] 1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,6 +15,12 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>
+        average{' '}
+        {(good + neutral * 0 + bad * -1) / (good + neutral + bad)}
+      </p>
+      <p>positive {good / (good + neutral + bad)} %</p>
     </div>
   );
 };
