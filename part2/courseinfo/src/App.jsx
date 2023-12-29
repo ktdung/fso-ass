87d31c9e05ed963a09ab3e73664abbe4f2e5a512
ktdung <ktdunglibre@gmail.com>
2023-12-29 08:56:20 +0700

    [courseinfo] 2.4* done

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h2>{props.course}</h2>;
 };
 
 const Part = (props) => {
@@ -43,35 +43,69 @@ const Total = ({ numOfExercises }) => {
   return <h3>total of {numOfExercises} exercises</h3>;
 };
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
+const Curriculum = ({ courses, name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      {courses.map((course) => {
+        return <Course key={course.id} course={course} />;
+      })}
+    </div>
+  );
+};
 
-  return <Course course={course} />;
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <Curriculum
+      courses={courses}
+      name={'Web development curriculum'}
+    />
+  );
 };
 
 export default App;
