e6a4fb7f7d9f14492f69e34c8ee5c03c5b4d7e17
ktdung <ktdunglibre@gmail.com>
2024-01-07 10:12:39 +0700

    3.13 Phonebook databse done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+
+const Person = require('./models/person');
 
 app.use(express.json());
 
@@ -21,32 +24,6 @@ app.use(cors());
 app.use(express.static('dist'));
 
 // app.use(morgan('tiny'));
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
-const generateId = () => {
-  return Math.floor(Math.random() * 10000000);
-};
 
 app.get('/info', (req, res) => {
   let date = new Date();
@@ -58,7 +35,9 @@ app.get('/info', (req, res) => {
 });
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get('/api/persons/:id', (req, res) => {
@@ -117,3 +96,26 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log('server listening on port: ', PORT);
 });
+
+// let persons = [
+//   {
+//     id: 1,
+//     name: 'Arto Hellas',
+//     number: '040-123456',
+//   },
+//   {
+//     id: 2,
+//     name: 'Ada Lovelace',
+//     number: '39-44-5323523',
+//   },
+//   {
+//     id: 3,
+//     name: 'Dan Abramov',
+//     number: '12-43-234345',
+//   },
+//   {
+//     id: 4,
+//     name: 'Mary Poppendieck',
+//     number: '39-23-6423122',
+//   },
+// ];
---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const url = process.env.MONGO_URL;
+
+console.log('connecting to ', url);
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB: ', error.message);
+  });
+
+const personScheme = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personScheme.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Person', personScheme);
