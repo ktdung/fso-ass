a32b023d88db21718fb6a7b37cf6e773d4827ac2
ktdung <ktdunglibre@gmail.com>
2024-01-12 17:25:04 +0700

    4.1 4.2 Blog list

---
+++ part4/blog/app.js
@@ -0,0 +1,44 @@
+const config = require('./utils/config');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blog.controller');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+
+// database init
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB');
+  });
+
+// logger
+logger.info('connecting to', config.MONGODB_URI);
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.static('dist'));
+app.use(middleware.requestLogger);
+
+console.log('hello');
+
+app.get('/info', (req, res) => {
+  console.log('1');
+  res.send({
+    sayHi: 'Hello World',
+  });
+});
+
+// router
+app.use('/api/blogs', blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
