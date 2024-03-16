8a584a7b5ddd30bb81e0b0b8ff3a49d927ce66bc
ktdung <ktdunglibre@gmail.com>
2024-03-16 16:16:51 +0700

    5.7 add ditail blog component

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [show, setShow] = React.useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleShowBlog = (event) => {
+    setShow(!show);
+  };
+  return (
+    <div style={blogStyle}>
+      <p>
+        {blog.title} {blog.author}{' '}
+        <button onClick={handleShowBlog}>
+          {show ? 'hide' : 'view'}
+        </button>
+      </p>
+      <div style={{ display: show ? '' : 'none' }}>
+        <p>
+          <a href={`${blog.url}`}>{blog.url}</a>
+        </p>
+        <p>
+          likes {blog.likes} <button>like</button>
+        </p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
