5ff3b58d9a1d08203c1679f2c117089743e46592
ktdung <ktdunglibre@gmail.com>
2024-03-23 10:20:52 +0700

    6.1 unicafe done

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
   useEffect(() => {
     setTimeout(() => {
       setMessage(null);
-    }, 10000);
+    }, 5000);
   }, [message]);
 
   const handleLogin = async (event) => {
